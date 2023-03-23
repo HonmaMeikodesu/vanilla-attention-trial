f6af9533d518024c4854fd462a5728680ae96f35
HonmaMeikodesu <whonmameikom@gmail.com>
2023-03-23 20:19:55 +0800

    recover: encoder and partial decoder

--- src/loss.py
+++ src/loss.py
@@ -5,8 +5,8 @@ from utils import sequence_mask
 #@save
 class MaskedSoftmaxCELoss(nn.CrossEntropyLoss):
     """带遮蔽的softmax交叉熵损失函数"""
-    # pred的形状：(batch_size,num_steps,vocab_size)
-    # label的形状：(batch_size,num_steps)
-    # valid_len的形状：(batch_size,)
     def forward(self, pred, label, valid_len):
+        # pred (batch_size, num_steps, vocab_size)
+        # label (batch_size, num_steps)
+        # valid_len(batch_size, int)
         pass
--- src/model.py
+++ src/model.py
@@ -9,23 +9,53 @@ class Seq2SeqEncoder(nn.Module):
     def __init__(self, vocab_size, embed_size, num_hiddens, num_layers,
                  dropout=0, **kwargs):
         super(Seq2SeqEncoder, self).__init__(**kwargs)
-        pass
+        self.emb = nn.Embedding(vocab_size, num_hiddens)
+        self.rnn = nn.GRU(embed_size, num_hiddens, num_layers, dropout=dropout)
 
     def forward(self, X, *args):
-        pass
+        # X (batch_size, num_steps, vocab)
+        X: torch.Tensor = self.emb(X)
+        # X (batch_size, num_steps, embed_size)
+        X = X.permute(1, 0, 2)
+        # X (num_steps, batch_size, embed_size)
+        output, hidden_state = self.rnn(X)
+        # output (batch_size, num_steps, num_hidden)
+        # hidden_state (num_layers, batch_size, num_hidden)
+        return output, hidden_state
 
 class Seq2SeqDecoder(nn.Module):
     """用于序列到序列学习的循环神经网络解码器"""
     def __init__(self, vocab_size, embed_size, num_hiddens, num_layers,
                  dropout=0, **kwargs):
         super(Seq2SeqDecoder, self).__init__(**kwargs)
-        pass
+        self.emb = nn.Embedding(vocab_size, num_hiddens) # 用于强制教学
+        self.rnn = nn.GRU(embed_size + num_hiddens, num_hiddens, num_layers, dropout=dropout)
+        self.ffn = nn.Linear(num_hiddens, vocab_size) # 最终决策层
 
     def init_state(self, enc_outputs, *args):
-        pass
+        __, hidden_state = enc_outputs
+        return hidden_state
 
     def forward(self, X, state):
-        pass
+        X: torch.Tensor = self.emb(X)
+        X = X.permute(1, 0, 2)
+        enc_context: torch.Tensor = self.init_state(state)[-1]
+        # X (num_steps, batch_size, embed_size)
+        # enc_context (batch_size, num_hidden)
+        enc_context = enc_context.repeat(X.shape[0], 1, 1) # 广播维度，为了与X的embed_size维进行concat
+        # enc_context (num_steps, batch_size, num_hidden)
+        X_concat_context = torch.concat(X, enc_context, dim=2)
+        # X_concat_context (num_steps, batch_size, num_hidden + embed_size)
+        
+        output, __ = self.rnn(X_concat_context)
+        # output (batch_size, num_steps, num_hidden)
+        # hidden_state (num_layers, batch_size, num_hidden)
+
+        final_output = self.ffn(output)
+        # final_output (batch_size, num_steps, vocal_size)
+        
+
+        
 
 class EncoderDecoder(nn.Module):
     """The base class for the encoder-decoder architecture.
